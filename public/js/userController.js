eb26a35c56ea5bacec53a2754fced11b085323a1
Jakub Wojtyra <wojtyraj@gmail.com>
2017-10-11 17:55:24 +0000

    add fetch

--- public/js/userController.js
+++ public/js/userController.js
@@ -3,6 +3,7 @@
     document.addEventListener("DOMContentLoaded", function(event) { 
         var displayName = document.querySelector('#display-name') || null;
         var url = window.location.origin + '/api';
+        /*
         var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.onload = function () {
@@ -17,6 +18,10 @@
             }
         }
       xhr.send();
+      */
+      window.fetch(url)
+            .then( res => res.json() )
+            .then( userObject => userObject.displayName !== null ? displayName.innerHTML = userObject['displayName'] : "" );
     });
     
 })();
